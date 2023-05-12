bb203a09c1d174eea8ac0d654dcdc6282abce530
MykolaIvanov1 <l0_t0s@ukr.net>
2023-05-12 17:45:12 +0300

    change see more

--- src/js/category.js
+++ src/js/category.js
@@ -75,7 +75,7 @@ function renderBooksList(books, event) {
   const markup = books
     .map(({ book_image, title, author, _id }) => {
       return `<li class = "category_books_items">
-          <div class="test-wraper" >
+          <div class="test-wraper">
           <img src='${book_image}' alt='book-cover' class='bestsellers-book-cover' data-id="${_id}">
           </div>
              <p class='bestsellers-book-title book-text'>${formatBookName(
