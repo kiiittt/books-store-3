b005296188db464cdc652a4ca60fef1e7a287222
MykolaIvanov1 <l0_t0s@ukr.net>
2023-05-12 11:39:13 +0300

    add modal js

--- src/js/modal.js
+++ src/js/modal.js
@@ -6,23 +6,27 @@
     // modalBackdrop: document.querySelector('[data-modal-backdrop]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModalOpen);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModalOpen);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
   // refs.modalBackdrop.addEventListener("click", toggleModal);
-  window.addEventListener("keydown", handleKeyDown);
+  window.addEventListener('keydown', handleKeyDown);
 
-  function toggleModalOpen() {
-    refs.modal.classList.toggle("is-hidden");
+  function toggleModalOpen(event) {
+    if (event.target.className !== 'bestsellers-book-cover') {
+      return;
+    }
+    refs.modal.classList.toggle('is-hidden');
+
     // document.body.style.overflow = refs.modal.classList.contains("is-hidden") ? "" : "hidden";
-}
+  }
 
-function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-}
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
 
   function handleKeyDown(event) {
-    if (event.key === "Escape") {
-      refs.modal.classList.add("is-hidden");
+    if (event.key === 'Escape') {
+      refs.modal.classList.add('is-hidden');
     }
   }
 })();
